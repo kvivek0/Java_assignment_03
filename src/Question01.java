2f5cf0e3fa82ff8c8e9b155fa20475b1d62f6931
kvivek0 <vivekksp963@gmail.com]>
2026-06-08 14:55:46 +0530

    First commit

---
+++ src/Question01.java
@@ -0,0 +1,12 @@
+/*
+   Write a program with one char type variable. Assign 'A' in the variable.
+     Now change the value of variable from 'A' to 'B' using increment operator.
+*/
+
+public class Question01 {
+    public static void main(String []args){
+        char A=65;
+             ++A;
+         System.out.println("After increment of A is "+A);
+    }
+}
