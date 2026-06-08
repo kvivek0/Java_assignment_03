21d9f5c798739b107a4a919d23cd43a3b2763265
kvivek0 <vivekksp963@gmail.com]>
2026-06-08 15:49:48 +0530

    added Q.5 and updated Q.4'

--- src/Question04.java
+++ src/Question04.java
@@ -2,13 +2,16 @@
 
 public class Question04 {
     public static void main(String []args){
-        int x=235,y,z;
+       /* int x=235,y,z;
           y=x%10; // here y contains 5 after applying modulus;
           x=x/10; // here x contain 23 after applying divide;
           z=x%10; // here z contains 3 after applying modulus;
           x=x/10; // here x contains 2 after applying divide;
              // allover:- x=2,y=3,z=5;
-          x=x+y+z;
+          x=x+y+z;*/
+         int x=234;
+         x=(x/100)+(x%10)+(x/10)%10;   //updated;
+
          System.out.print("sum of digit is "+x); // summation
     }
 }
---
+++ src/Question05.java
@@ -0,0 +1,14 @@
+//5. What will be the result of an expression - 5>4>3
+
+public class Question05 {
+    public static void main(String []args){
+      int x=5,y=4,z=3;
+         //  x>y>z;
+         /* sol:- operand start compiling from right side,
+           then comparing 4 is greater than 3 and that give a boolean
+           answer true and boolean then compare with integer 5
+           "That Gives A Invalid Operand"
+          */
+
+    }
+}
