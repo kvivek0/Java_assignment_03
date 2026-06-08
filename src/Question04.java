bf1c9b4392ab0392de80cb5a7e2d4b9756268d6e
kvivek0 <vivekksp963@gmail.com]>
2026-06-08 15:36:33 +0530

    added Q.4

---
+++ src/Question04.java
@@ -0,0 +1,14 @@
+//Write a program to calculate sum of the digits of a given number.
+
+public class Question04 {
+    public static void main(String []args){
+        int x=235,y,z;
+          y=x%10; // here y contains 5 after applying modulus;
+          x=x/10; // here x contain 23 after applying divide;
+          z=x%10; // here z contains 3 after applying modulus;
+          x=x/10; // here x contains 2 after applying divide;
+             // allover:- x=2,y=3,z=5;
+          x=x+y+z;
+         System.out.print("sum of digit is "+x); // summation
+    }
+}
